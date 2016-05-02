f0282a6d675e3261df349209d7efe9fae2fcd281
Jack.Yj.Din <jack.yj.din@benq.com>
2016-05-02 20:35:53 +0800

    0502  textField delegate

--- PeterPan_0502/ViewController.swift
+++ PeterPan_0502/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
+    @IBOutlet weak var textF: UITextField!
+    @IBOutlet weak var textCountLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK: TextFieldDelegate
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        
+        guard let text = textField.text else { return true }
+        let newLength = text.utf16.count + string.utf16.count - range.length
+        textCountLabel.text = "\(newLength)"
+        return newLength <= 10 // Bool
+        
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        self.view.endEditing(true)
+        return true
+    }
 
 }
 
